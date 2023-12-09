112a9510f6a73032b8821685be94fb52ce752027
AgnesaS <agnesaselimi8@gmail.com>
2023-12-09 22:13:54 +0100

    Initial Commit

---
+++ LandmarkApp/LandmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkAppApp.swift
+//  LandmarkApp
+//
+//  Created by Agnesa  on 9.12.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
